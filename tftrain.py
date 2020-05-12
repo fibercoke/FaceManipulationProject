359402a84f39a99c879e0e21a27ad29053000bac
MagHSK <maghsk2017@gmail.com>
2020-05-12 10:40:08 +0800

    Keras implementation

--- tftrain.py
+++ tftrain.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
-import argparse
 import logging
-import os.path as osp
+from time import strftime
+
+import logging
 from time import strftime
 
 import tensorflow as tf
@@ -14,165 +12,9 @@ from tensorflow.python.keras.layers import GlobalAveragePooling2D, Flatten, Dens
 
 from tf_vdc_loss import VDCLoss
 from tfrecord_dataset_utils import parse_tfrecord
-from utils.tfddfa import str2bool
-from utils.tfio import mkdir
-
-# global args (configuration)
-args = None
-lr = None
-arch_choices = ['mobilenet_2', 'mobilenet_1', 'mobilenet_075', 'mobilenet_05', 'mobilenet_025']
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='3DMM Fitting')
-    parser.add_argument('-j', '--workers', default=6, type=int)
-    parser.add_argument('--epochs', default=40, type=int)
-    parser.add_argument('--start-epoch', default=1, type=int)
-    parser.add_argument('-b', '--batch-size', default=128, type=int)
-    parser.add_argument('-vb', '--val-batch-size', default=32, type=int)
-    parser.add_argument('--base-lr', '--learning-rate', default=0.001, type=float)
-    parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float)
-    parser.add_argument('--print-freq', '-p', default=20, type=int)
-    parser.add_argument('--resume', default='', type=str, metavar='PATH')
-    parser.add_argument('--devices-id', default='0,1', type=str)
-    parser.add_argument('--filelists-train',
-                        default='', type=str)
-    parser.add_argument('--filelists-val',
-                        default='', type=str)
-    parser.add_argument('--root', default='')
-    parser.add_argument('--snapshot', default='', type=str)
-    parser.add_argument('--log-file', default='output.log', type=str)
-    parser.add_argument('--log-mode', default='w', type=str)
-    parser.add_argument('--size-average', default='true', type=str2bool)
-    parser.add_argument('--num-classes', default=62, type=int)
-    parser.add_argument('--arch', default='mobilenet_1', type=str,
-                        choices=arch_choices)
-    parser.add_argument('--frozen', default='false', type=str2bool)
-    parser.add_argument('--milestones', default='15,25,30', type=str)
-    parser.add_argument('--task', default='all', type=str)
-    parser.add_argument('--test_initial', default='false', type=str2bool)
-    parser.add_argument('--warmup', default=-1, type=int)
-    parser.add_argument('--param-fp-train',
-                        default='',
-                        type=str)
-    parser.add_argument('--param-fp-val',
-                        default='')
-    parser.add_argument('--opt-style', default='resample', type=str)  # resample
-    parser.add_argument('--resample-num', default=132, type=int)
-    parser.add_argument('--loss', default='vdc', type=str)
-
-    global args
-    args = parser.parse_args()
-
-    # some other operations
-    args.devices_id = [int(d) for d in args.devices_id.split(',')]
-    args.milestones = [int(m) for m in args.milestones.split(',')]
-
-    snapshot_dir = osp.split(args.snapshot)[0]
-    mkdir(snapshot_dir)
-
-
-def print_args(args):
-    for arg in vars(args):
-        s = arg + ': ' + str(getattr(args, arg))
-        logging.info(s)
-
-
-def adjust_learning_rate(optimizer, epoch, milestones=None):
-    """Sets the learning rate: milestone is a list/tuple"""
-
-    def to(epoch):
-        if epoch <= args.warmup:
-            return 1
-        elif args.warmup < epoch <= milestones[0]:
-            return 0
-        for i in range(1, len(milestones)):
-            if milestones[i - 1] < epoch <= milestones[i]:
-                return i
-        return len(milestones)
-
-    n = to(epoch)
-
-    global lr
-    lr = args.base_lr * (0.2 ** n)
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-
-def save_checkpoint(model, filename='checkpoint.pth.tar'):
-    model.save(filename)
-    logging.info(f'Save checkpoint to {filename}')
-
-
-"""
-def train(train_loader, model, criterion, optimizer, epoch):
-    model.train()
-
-    end = time.time()
-    # loader is batch style
-    # for i, (input, target) in enumerate(train_loader):
-    for i, (input, target) in enumerate(train_loader):
-        logging.info("Input shape:", str(input.shape))
-        target.requires_grad = False
-        target = target.cuda(non_blocking=True)
-        output = model(input)
-
-        data_time.update(time.time() - end)
-
-        if args.loss.lower() == 'vdc':
-            loss = criterion(output, target)
-        elif args.loss.lower() == 'wpdc':
-            loss = criterion(output, target)
-        elif args.loss.lower() == 'pdc':
-            loss = criterion(output, target)
-        else:
-            raise Exception(f'Unknown loss {args.loss}')
-
-        losses.update(loss.item(), input.size(0))
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        # log
-        if i % args.print_freq == 0:
-            logging.info(f'Epoch: [{epoch}][{i}/{len(train_loader)}]\t'
-                         f'LR: {lr:8f}\t'
-                         f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                         # f'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                         f'Loss {losses.val:.4f} ({losses.avg:.4f})')
-
-
-def validate(val_loader, model, criterion, epoch):
-    model.eval()
-
-    end = time.time()
-    with torch.no_grad():
-        losses = []
-        for i, (input, target) in enumerate(val_loader):
-            # compute output
-            target.requires_grad = False
-            target = target.cuda(non_blocking=True)
-            output = model(input)
-
-            loss = criterion(output, target)
-            losses.append(loss.item())
-
-        elapse = time.time() - end
-        loss = np.mean(losses)
-        logging.info(f'Val: [{epoch}][{len(val_loader)}]\t'
-                     f'Loss {loss:.4f}\t'
-                     f'Time {elapse:.3f}')
-
-"""
 
 
 def main():
-    parse_args()  # parse global argsl
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     for physical_device in physical_devices:
         tf.config.experimental.set_memory_growth(physical_device, True)
@@ -182,32 +24,33 @@ def main():
         format='[%(asctime)s] [p%(process)s] [%(pathname)s:%(lineno)d] [%(levelname)s] %(message)s',
         level=logging.INFO,
         handlers=[
-            logging.FileHandler(args.log_file, mode=args.log_mode),
+            logging.FileHandler('output.log', mode='w'),
             logging.StreamHandler()
         ]
     )
 
-    print_args(args)  # print args
-
     # step1: define the model structure
-    # model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(120, 120, 3), alpha=1.0, include_top=True,
-    #                                                    weights=None, input_tensor=None, pooling=None,
-    #                                                    classes=62)
+    use_imagenet = True
+    if use_imagenet:
+        base_model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(120, 120, 3), alpha=1.0,
+                                                                 include_top=False,
+                                                                 weights='imagenet', input_tensor=None, pooling=None)
+        base_model.summary()
 
-    base_model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(120, 120, 3), alpha=1.0, include_top=False,
-                                                             weights='imagenet', input_tensor=None, pooling=None)
-    base_model.summary()
+        for layer in base_model.layers:
+            layer.trainable = False
 
-    for layer in base_model.layers:
-        layer.trainable = False
+        model = Sequential()
+        model.add(base_model)
+        model.add(GlobalAveragePooling2D())
+        model.add(Flatten())
+        model.add(Dense(512, activation='relu'))
+        # model.add(Dropout(0.1))
+        model.add(Dense(62, activation='softmax'))
+    else:
+        model = keras.applications.mobilenet_v2.MobileNetV2(input_shape=(120, 120, 3), alpha=1.0, include_top=True,
+                                                            weights=None, input_tensor=None, pooling=None, classes=62)
 
-    model = Sequential()
-    model.add(base_model)
-    model.add(GlobalAveragePooling2D())
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    # model.add(Dropout(0.1))
-    model.add(Dense(62, activation='softmax'))
     model.summary()
     # step2: optimization: loss and optimization method
     criterion = VDCLoss()
@@ -263,8 +106,6 @@ def main():
     # model.load_weights('checkpoints/xps_first').expect_partial()
 
     # step4: run
-    if args.test_initial:
-        logging.info('Testing from initial')
 
     time_str = strftime("%Y-%m-%d-%H%M%S")
     callbacks = [
@@ -278,7 +119,7 @@ def main():
     model.compile(optimizer=optimizer, loss=criterion)
     model.fit(train_dataset,
               validation_data=val_dataset,
-              epochs=args.epochs,
+              epochs=100,
               callbacks=callbacks,
               workers=8,
               initial_epoch=0)
